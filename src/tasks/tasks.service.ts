f0e6e66586d449fe609d57938395054db92fbb67
MarFilip456 <marcin.filipkowski64@gmail.com>
2022-08-19 16:45:47 +0200

    Almost finished section 9, ConfigService not getting data from .env.stage.dev

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -14,6 +14,7 @@ export class TasksService {
         private tasksRepository: TasksRepository,
     ) {}
 
+
     getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
         return this.tasksRepository.getTasks(filterDto, user);
     }
